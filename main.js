54a1eaa1bd42005f75c5c2eed09625de052cb927
MohamedAhmedElashry <154823673+MohamedAhmedElashry@users.noreply.github.com>
2023-12-25 18:12:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+window.alert("You'r life matter, If you are thinking about suicide please call or text 988") ;
+
+console.log("فلنبدأ ب بسم الله الرحمن الرحيم والحمدلله رب العالمين والصلاة والسلام علي اشرف المرسلين سيدنا محمد وعلي اله وصحبه اجمعين")
+document.querySelector("h1").style.color="Black"
+document.querySelector("h1").style.fontSize="30px"
+
